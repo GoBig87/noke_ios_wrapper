14d72a8cabd10fa33c556bd4f992825cb03ecf2f
GoBig87 <nobody5390@yahoo.com>
2018-07-06 20:06:06 -0700

    debug

--- Noke_Wrapper.pyx
+++ Noke_Wrapper.pyx
@@ -1,13 +1,14 @@
 cdef extern from "Noke_Wrapper_imp.h":
     ctypedef void (*callbackfunc)(const char *name, void *user_data)
-    ctypedef char (*clientfunc)(const char *session, const char *mac, void *util)
+    ctypedef char* (clientfunc)(const char *session, const char *mac, void *util)
     void request_Unlock(char* macChar, callbackfunc call_back, clientfunc client_func, void *user_data)
 
 def requestUnlock(util,mac):
 
-    cdef char* macChar = mac.encode('utf-8')
+    #cdef bytes mac_bytes = mac.encode('utf-8')
+    cdef bytes mac_bytes = mac.encode('utf-8')
 
-    request_Unlock(macChar, callback, reqTokenFunc, <void*>util)
+    request_Unlock(mac_bytes, callback, reqTokenFunc, <void*>util)
 
 cdef void callback(const char *name, void *util):
     (<object> util).NokeCallback = (name.decode('utf-8'))
