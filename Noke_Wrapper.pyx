57123d7892bb405bce14741f19e485027bcc3e28
GoBig87 <nobody5390@yahoo.com>
2018-07-08 13:29:31 -0700

    debug

--- Noke_Wrapper.pyx
+++ Noke_Wrapper.pyx
@@ -13,7 +13,7 @@ def requestUnlock(util,mac):
 cdef void callback(const char *name, void *util):
     (<object> util).NokeCallback = (name.decode('utf-8'))
 
-cdef char* reqTokenFunc(const char *session, const char *mac, void *util):
+cdef char reqTokenFunc(const char *session, const char *mac, void *util):
     #cdef char* rsp_char
     rsp = (<object> util).sendNokeMessage(session.decode('utf-8'),mac.decode('utf-8'))
     if rsp:
