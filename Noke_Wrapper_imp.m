ceb547a32f29b3137310585da7ad51b9c802f33f
GoBig87 <nobody5390@yahoo.com>
2018-07-07 20:16:53 -0700

    debug

--- Noke_Wrapper_imp.m
+++ Noke_Wrapper_imp.m
@@ -12,14 +12,14 @@
 
     NSString* NSlockMacAddr = [NSString stringWithUTF8String:lockMacAddr];
 
-    NokeDeviceManager *noke_device_manager = [[NokeDeviceManager alloc]init];
-    //[[NokeDeviceManager sharedInstance] setDelegate:self];
+    //NokeDeviceManager *noke_device_manager = [[NokeDeviceManager alloc]init];
+    [[NokeDeviceManager sharedInstance] setDelegate:self];
     //Set api key
     NSString* apiKey = @"debug";
-    [noke_device_manager setAPIKey:apiKey];
+    [NokeDeviceManager setAPIKey:apiKey];
     //Set upload url
     NSString* uploadUrl = @"https://coreapi-sandbox.appspot.com/upload/";
-    [noke_device_manager changeDefaultUploadUrl:uploadUrl];
+    [NokeDeviceManager changeDefaultUploadUrl:uploadUrl];
     //Set up lock
     NSString* lockName = @"lock Name";
     NokeDevice* noke = [[NokeDevice alloc]init name:lockName mac:NSlockMacAddr];
