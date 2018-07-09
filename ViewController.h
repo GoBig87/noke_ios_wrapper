2205751199163ec6f21b0828bfba5e6c0e10c121
GoBig87 <nobody5390@yahoo.com>
2018-07-08 17:12:24 -0700

    debug

--- ViewController.h
+++ ViewController.h
@@ -4,10 +4,13 @@
 typedef void (*callbackfunc) (const char *name, void *callback);
 typedef const char* (clientfunc) (const char *session, const char *macAddr, void *reqTokenFunc);
 
-@interface NokeViewController : NSObject
+@interface NokeViewController : NSObject{
+    clientfunc client_func;
+    callbackfunc callback;
+}
 
 @property (nonatomic, assign) clientfunc client_func;
-@property (nonatomic, assign) callbackfunc callback; 
+@property (nonatomic, assign) callbackfunc callback;
 @property (void*) util;
 
 - (NokeViewController *) init:(callbackfunc)callback client_func:(clientfunc)client_func util:(void*)util;
