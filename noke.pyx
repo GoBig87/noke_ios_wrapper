edfe56593bbefc8b4615006390b6393f0ea2e990
GoBig87 <nobody5390@yahoo.com>
2018-07-12 22:39:44 -0700

    debug

--- noke.pyx
+++ noke.pyx
@@ -4,15 +4,15 @@ cdef extern from "NokeViewController.h":
     ctypedef void (*store_viewcontroller)(void *viewcontroller,void *util)
     ctypedef void (*callbackfunc)(const char *name, void *user_data)
     ctypedef const char* (*clientfunc)(const char *session, const char *mac, void *util)
-    void StartUnlock(char* name, char* macChar, callbackfunc call_back, clientfunc client_func, void *user_data)
+    void StartUnlock(char* name, char* macChar, callbackfunc call_back, clientfunc client_func,store_viewcontroller storeviewcontroller, void *user_data)
 
 def requestUnlock(util,name,mac):
     cdef bytes name_bytes = name.encode('utf-8')
     cdef bytes mac_bytes  = mac.encode('utf-8')
 
-    StartUnlock(name_bytes,mac_bytes, callback, reqTokenFunc, store_viewcontroller, <void*>util)
+    StartUnlock(name_bytes,mac_bytes, callback, reqTokenFunc, storeviewcontroller, <void*>util)
 
-cdef void store_viewcontroller(void *viewcontroller,void *util):
+cdef void storeviewcontroller(void *viewcontroller,void *util):
     (<object> util).NokeViewController = (<object> viewcontroller)
 
 cdef void callback(const char *name, void *util):
