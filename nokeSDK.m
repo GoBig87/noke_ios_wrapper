62e764ce113c01b453663645f761743f6271c728
GoBig87 <nobody5390@yahoo.com>
2018-07-12 23:06:40 -0700

    debug

--- nokeSDK.m
+++ nokeSDK.m
@@ -221,7 +221,7 @@ static nokeSDK *sharedNokeSDK;
         [_delegate isBluetoothEnabled:YES];
         [self retrieveKnownPeripherals];
         _bluetoothState = YES;
-        [self startScanForNokeDevices];
+        //[self startScanForNokeDevices];
     }
     else
     {
