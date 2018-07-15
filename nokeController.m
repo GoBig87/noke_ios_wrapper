1c31a68f796f9e088c1c156f4340810ad35b95ed
GoBig87 <nobody5390@yahoo.com>
2018-07-15 15:13:36 -0700

    debug

---
+++ nokeController.m
@@ -0,0 +1,82 @@
+#import <LocalAuthentication/LocalAuthentication.h>
+
+@interface NokeController ()
+
+@end
+
+@implementation NokeController
+
+@synthesize mCallback = _callback;
+@synthesize mUtil = _util;
+@synthesize mClient = _client;
+
+
+static NokeController *nokeController;
+
++ (NokeController*) sharedInstance
+{
+    if(nokeController == nil)
+    {
+        nokeController = [[NokeController alloc] init];
+    }
+    return nokeController;
+}
+-(void) startNokeScan:(char*)name mac:(char*)lockMacAddr callback:(callbackfunc)callback client_func:(clientfunc)client_func viewcontroller:(store_viewcontroller)viewcontroller util:(void*)util{
+    _callback = callback;
+    _util = util;
+    _client = client_func;
+
+    NSLog(@"Debug-Noke-1");
+    NSString* NSlockMacAddr = [NSString stringWithUTF8String:lockMacAddr];
+    NSString* NSname = [NSString stringWithUTF8String:name];
+    NSLog(@"Debug-Noke-2");
+    nokeDevice *noke = [[nokeDevice alloc] initWithName:NSname Mac:NSlockMacAddr];
+    //Hard coding this in nokeClient
+    //[nokeClient setToken:'my token here'];
+    [[nokeSDK sharedInstance] insertNokeDevice:noke];
+    //Start scanning
+    NSLog(@"Debug-Noke-3");
+    //Scanning must start after bluetooth is enabled!!
+    [[nokeSDK sharedInstance] startScanForNokeDevices];
+}
+
+ -(void) isBluetoothEnabled:(bool)enabled
+{
+    if(enabled){
+        NSLog(@"Bluetooth enabled");
+    }else{
+        NSLog(@"Bluetooth disabled");
+    }
+    //Called when bluetooth is enabled or disabled
+}
+
+-(void) didDiscoverNokeDevice:(nokeDevice*)noke
+{
+    NSLog(@"Lock Discovered");
+    //Is called when a noke device is discovered.
+}
+
+-(void) didConnect:(nokeDevice*) noke
+{
+    NSLog(@"Lock Discovered");
+    //Called when a noke device has successfully connected to the app
+}
+
+-(void) didDisconnect:(nokeDevice*) noke
+{
+    NSLog(@"Lock Disconnected");
+    //Called after a noke device has been disconnected
+}
+
+-(void) didReceiveData:(NSData*)data
+{
+    NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    const char *chars = [string UTF8String];
+    NSLog(string)
+    NSLog(@"Data ready");
+    //Called when the lock sends back data that needs to be passed to the server
+}
+
+void StartUnlock(char* name, char* lockMacAddr,callbackfunc callback, clientfunc client_func,store_viewcontroller viewcontroller, void *util){
+    [[NokeViewController sharedInstance] startNokeScan:name mac:lockMacAddr callback:callback client_func:client_func viewcontroller:viewcontroller util:util];
+}
