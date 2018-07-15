cac27aa374353a1aa4b3407a595d4f3fa5950596
GoBig87 <nobody5390@yahoo.com>
2018-07-15 16:34:03 -0700

    debug

--- NokeController.m
+++ NokeController.m
@@ -62,12 +62,13 @@ static NokeController *nokeController;
 -(void) didDiscoverNokeDevice:(nokeDevice*)noke RSSI:(NSNumber*)RSSI
 {
     NSLog(@"Lock Discovered");
+    //[[nokeSDK sharedInstance] connectToNokeDevice:noke];
     //Is called when a noke device is discovered.
 }
 
 -(void) didConnect:(nokeDevice*) noke
 {
-    NSLog(@"Lock Discovered");
+    NSLog(@"Lock Connected");
     //Called when a noke device has successfully connected to the app
 }
 
