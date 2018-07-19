be05b61900414d8e8769fced73c91a7fdbf8b7ed
GoBig87 <nobody5390@yahoo.com>
2018-07-18 18:50:40 -0700

    debug

--- noke.pyx
+++ noke.pyx
@@ -20,6 +20,8 @@ cdef void callback(const char *name, void *util):
     (<object> util).NokeCallback = (name.decode('utf-8'))
 
 cdef const char* reqTokenFunc(const char *session, const char *mac, void *util):
+    sessionStr = (session.decode('utf-8'))
+    macStr     = (mac.decode('utf-8'))
     (<object> util).sendNokeMessage(sessionStr,macStr)
     #rsp = (<object> util).NokeCallback
     #printf("%s\n", session)
