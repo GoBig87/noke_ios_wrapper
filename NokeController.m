6b0658e4d7ea3b05ff3e8b4a2bcb1e381af2975e
GoBig87 <nobody5390@yahoo.com>
2018-07-18 21:22:05 -0700

    debug

--- NokeController.m
+++ NokeController.m
@@ -162,7 +162,7 @@ static NokeCallback *nokeCallback;
 + (NSString*) sendTokenToServer:(NSString*)session mac:(NSString*)mac{
     const char *charDeeMacDennis = [mac UTF8String];
     const char *sessionChar = [session UTF8String];
-    const char *rspChar = _nClient(sessionChar,charDeeMacDennis,_nUtil);
+    const char *rspChar = _Client(sessionChar,charDeeMacDennis,_Util);
     NSString* rsp = [NSString stringWithUTF8String:rspChar];
     return rsp;
 }
