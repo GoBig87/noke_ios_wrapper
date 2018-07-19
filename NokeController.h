483b465ed9d0f814aa1999120f70ae433259dea2
GoBig87 <nobody5390@yahoo.com>
2018-07-18 21:11:41 -0700

    debug

--- NokeController.h
+++ NokeController.h
@@ -37,9 +37,9 @@ typedef void(^myCompletion)(NSString*);
 
 @interface NokeCallback : NSObject
 
-@property (nonatomic, assign) clientfunc mClient;
-@property (nonatomic, assign) callbackfunc mCallback;
-@property (nonatomic, assign) void* mUtil;
+@property (nonatomic, assign) clientfunc nClient;
+@property (nonatomic, assign) callbackfunc nCallback;
+@property (nonatomic, assign) void* nUtil;
 
 +(NokeCallback*) sharedInstance;
 +(NSString*) sendTokenToServer:(NSString*)session mac:(NSString*)mac;
