2e020352dee9f4542fe7a5a96695e51601e6b679
GoBig87 <nobody5390@yahoo.com>
2020-06-13 18:18:18 -0700

    attempting to fix noke

--- NokeController.m
+++ NokeController.m
@@ -29,7 +29,7 @@ static NokeController *nokeController;
     }
     return nokeController;
 }
--(void) startNokeScan:(char*)name mac:(char*)lockMacAddr lockState:(bool)lockState callback:(callbackfunc)callback client_func:(clientfunc)client_func blockunlock_func:(blockunlockfunc)blockunlock_func util:(void*)util{
+-(void) startNokeScan:(char*)name mac:(char*)lockMacAddr lockState:(bool)lockState callback:(callbackfunc)callback client_func:(clientfunc)client_func util:(void*)util{
 
     _callback = callback;
     _util = util;
